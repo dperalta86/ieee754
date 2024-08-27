e469c893a699ac774f64463d0447ab12ad0cbd7b
Daniel Peralta <dp25443@gmail.com>
2024-08-26 23:11:40 -0300

    Se modificaron algunos estilos css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => (
     <div className="header">
-        <img src="/images/image.png" alt="Descripción" className="header-image" />
+        <img src="/images/image.png" alt="Descripción" className="heroImage" />
         <p>Esta aplicación permite convertir números float a su representación IEEE 754 (formato simple) y viceversa.</p>
     </div>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Header from '../components/Header';
 import Tabs from '../components/Tabs';
 import Sidebar from '../components/Sidebar';
 
+// Importa el módulo CSS
+import styles from '../styles/home.module.css';
+
 export default function Home() {
   return (
       <div>
@@ -12,53 +15,23 @@ export default function Home() {
               <link rel="icon" href="/favicon.ico"/>
           </Head>
 
-          <div className="container">
+          <div className={styles.container}>
               {/* Barra lateral izquierda */}
-              <aside className="sidebar">
+              <aside className={styles.sidebar}>
                   <Sidebar content="left"/>
               </aside>
 
               {/* Contenido principal */}
-              <main className="main-content">
+              <main className={styles.mainContent}>
                   <Header/>
                   <Tabs/>
               </main>
 
               {/* Barra lateral derecha */}
-              <aside className="sidebar">
+              <aside className={styles.sidebar}>
                   <Sidebar content="right"/>
               </aside>
           </div>
-          <style jsx>{`
-              .container {
-                  display: flex;
-                  flex-wrap: wrap;
-              }
-
-              .sidebar {
-                  flex: 1;
-                  min-width: 200px;
-                  background-color: #f0f0f0;
-                  padding: 20px;
-              }
-
-              .main-content {
-                  flex: 2;
-                  min-width: 300px;
-                  padding: 20px;
-              }
-
-              @media screen and (max-width: 768px) {
-                  .sidebar {
-                      display: none; /* Ocultar las barras laterales en pantallas pequeñas */
-                  }
-
-                  .main-content {
-                      flex: 1;
-                      width: 100%;
-                  }
-              }
-          `}</style>
       </div>
   );
 }
