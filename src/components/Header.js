94fca1edf5bc4cddab9284eaf5d06348a27a0ac4
Daniel Peralta <dp25443@gmail.com>
2024-08-26 22:44:49 -0300

    Se agregó imagen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => (
     <div className="header">
-        <img src="/path-to-image.jpg" alt="Descripción" className="header-image" />
+        <img src="/images/image.png" alt="Descripción" className="header-image" />
         <p>Esta aplicación permite convertir números float a su representación IEEE 754 (formato simple) y viceversa.</p>
     </div>
 );
