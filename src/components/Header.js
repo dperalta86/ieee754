c852ddbb040bb040b6a61e85232983f6a932f493
Daniel Peralta <dp25443@gmail.com>
2024-08-30 12:34:09 -0300

    Se "limpia" error después de un NaN. Se agreggó estilos a cadena de bits

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ const Header = () => (
     <div className="header">
         <img src="/images/image.png" alt="Descripción" className="heroImage" />
         <p>Esta aplicación permite convertir números float a su representación IEEE 754 (formato simple) y viceversa.</p>
+        <p>Esta herramienta muestra el formato utilizado por la mayoria de los CPU's en la actualidad. Acepta los
+         siguientes valores especiales: NaN (Not a number), Infinity, -Infinity.</p>
     </div>
 );
 
